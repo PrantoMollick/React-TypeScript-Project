077ee6575b9966da23839c63bd030cf7b44a3588
Pranto <prantomollick01@gmail.com>
2023-08-22 17:58:06 +0600

    display line chart rechart library

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -4,6 +4,7 @@ import Spinner from '../../ui/Spinner';
 import { useRecentStays } from './useRecentStays';
 import Stats from './Stats';
 import useCabins from '../cabins/useCabins';
+import SalesChart from './SalesChart';
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -30,7 +31,7 @@ function DashboardLayout() {
       <div>Statistics</div>
       <div>Today&apos;s Activity</div>
       <div>Chart stay durations</div>
-      <div>Chart sales</div>
+      <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   );
 }
