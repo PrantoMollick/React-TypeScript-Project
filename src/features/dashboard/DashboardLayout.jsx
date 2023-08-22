ce29515267c368780687f838d513dcafb069cdc8
Pranto <prantomollick01@gmail.com>
2023-08-22 18:37:07 +0600

    display piechart

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -5,6 +5,7 @@ import { useRecentStays } from './useRecentStays';
 import Stats from './Stats';
 import useCabins from '../cabins/useCabins';
 import SalesChart from './SalesChart';
+import DurationChart from './DurationChart';
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -30,7 +31,7 @@ function DashboardLayout() {
       />
       <div>Statistics</div>
       <div>Today&apos;s Activity</div>
-      <div>Chart stay durations</div>
+      <DurationChart confirmedStays={confirmedStays} />
       <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   );
