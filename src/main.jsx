2f23e888389fb9853779c8a093e611baaa6fc323
Pranto Mollick <prantocse01@gmail.com>
2023-11-20 00:51:12 +0600

    data updated

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { ErrorBoundary } from 'react-error-boundary';
 import ErrorFallback from './ui/ErrorFallback.jsx';
+import Uploader from './data/Uploader.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
